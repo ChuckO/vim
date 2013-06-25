75b3075f349580ebcf10b493e2c2aea3d6b09a66
Chuck <chuck@cloud.com>
2013-06-25 09:52:00 -0400

    latest

--- update_bundles.rb
+++ update_bundles.rb
@@ -4,26 +4,37 @@
 # http://efiquest.org/2010-09-24/49/
 
 git_bundles = [ 
-  "git://github.com/msanders/snipmate.vim.git",
   "git://github.com/scrooloose/nerdtree.git",
-  "git://github.com/tpope/vim-markdown.git",
-  "git://github.com/tpope/vim-rails.git",
-  "git://github.com/vim-ruby/vim-ruby.git",
-  "git://github.com/rson/vim-conque",
+  "git://github.com/vim-scripts/tComment.git",
   "git://github.com/vim-scripts/bufkill.vim",
   "git://github.com/vim-scripts/L9",
   "git://github.com/vim-scripts/FuzzyFinder.git",
-  "git://github.com/tpope/vim-fugitive.git",
-  "git://github.com/tyru/open-browser.vim",
-  "git://github.com/vim-scripts/AsyncCommand",
   "git://github.com/tpope/vim-surround.git",
+  "git://github.com/flazz/vim-colorschemes.git",
+  "git://github.com/ervandew/supertab.git",
+  "git://github.com/vim-scripts/ZoomWin.git",
+
+# No dice with leader, need to figure leader out
+#  "git://github.com/Lokaltog/vim-easymotion.git",
+  
+# doesn't work iwith ruby 1.9 hash syntax but good idea
+#  "git://github.com/scrooloose/syntastic.git",
+
+#  "git://github.com/vim-scripts/AsyncCommand",
+#  "git://github.com/scrooloose/nerdcommenter.git",
+#  "git://github.com/tpope/vim-haml.git",
+#  "git://github.com/msanders/snipmate.vim.git",
+#  "git://github.com/tpope/vim-markdown.git",
+#  "git://github.com/tpope/vim-rails.git",
+#  "git://github.com/vim-ruby/vim-ruby.git",
+#  "git://github.com/rson/vim-conque",
+#  "git://github.com/tpope/vim-fugitive.git",
+#  "git://github.com/tyru/open-browser.vim",
 #  "https://github.com/vim-scripts/xml.vim.git",
 #  "git://github.com/timcharper/textile.vim.git",
 #  "git://github.com/tpope/vim-git.git",
-#  "git://github.com/flazz/vim-colorschemes.git",
-##  "git://github.com/astashov/vim-ruby-debugger.git",
+#  "git://github.com/astashov/vim-ruby-debugger.git",
 #  "git://github.com/tpope/vim-cucumber.git",
-#  "git://github.com/tpope/vim-haml.git",
 #  "git://github.com/tpope/vim-repeat.git",
 #  "git://github.com/tsaleh/vim-align.git",
 #  "git://github.com/tsaleh/vim-shoulda.git",
@@ -32,11 +43,11 @@ git_bundles = [
 ]
 
 vim_org_scripts = [
-  ["IndexedSearch", "7062",  "plugin"],
-#  ["gist",          "12732", "plugin"],
-  ["jquery",        "12107", "syntax"],
   ["railscasts",    "8379",  "colors"],
   ["bufonly",       "3388",  "plugin"],
+#  ["IndexedSearch", "7062",  "plugin"],
+#  ["gist",          "12732", "plugin"],
+#  ["jquery",        "12107", "syntax"],
 ]
 
 require 'fileutils'
